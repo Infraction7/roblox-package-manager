e1a4ff55e82bf46a5f7318a32b5bca7b46f3cc88
Infraction7 <112822467+Infraction7@users.noreply.github.com>
2024-04-26 04:30:25 +0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,9 +1,8 @@
-@echo OFF
 if (Test-Path "./dist") {
-	rojo build -o ./dist/RPM.rbxmx &>null
-	copy -Force ./dist/RPM.rbxmx $ENV:LOCALAPPDATA\Roblox\Plugins\ &>null
+	rojo build -o ./dist/RPM.rbxmx
+	copy -Force ./dist/RPM.rbxmx $ENV:LOCALAPPDATA\Roblox\Plugins\
 } else {
 	mkdir dist
-	rojo build -o ./bin/RPM.rbxmx &>null
-	copy -Force ./bin/RPM.rbxmx $ENV:LOCALAPPDATA\Roblox\Plugins\ &>null
+	rojo build -o ./dist/RPM.rbxmx
+	copy -Force ./dist/RPM.rbxmx $ENV:LOCALAPPDATA\Roblox\Plugins\
 }
